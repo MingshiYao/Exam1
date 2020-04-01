b0772d6afaba3b011fb43367db17785a7c1ff89c
MingshiYao <myao@gus.pittstate.edu>
2020-03-31 23:04:53 -0500

    A message about my changes

--- decayFunc.py
+++ decayFunc.py
@@ -10,6 +10,8 @@ import numpy as np
 import math
 def decayFunc(N,lambd):
     t = 0
+    Nwitht=[0]
+    DeltaNwitht=[0]
     while N > 0:
         DeltaN = 0
         for i in range(N):
@@ -17,18 +19,35 @@ def decayFunc(N,lambd):
                 DeltaN = DeltaN + 1
         t = t + 1
         N = N - DeltaN
-    return t
-
+        if (N>0):
+            Nwitht[t-1]=math.log(N)
+        Nwitht=Nwitht+[0]
+        if (DeltaN>0):
+            DeltaNwitht[t-1]=math.log(DeltaN)
+        DeltaNwitht=DeltaNwitht+[0]
+    return t,DeltaNwitht,Nwitht
+print(decayFunc(10,0.005)) #test
+[a,b,c]=decayFunc(10,0.005) #test
+###problem solving
 lambd=0.005
-t=[0]*10000 # Array with 1000 elements 
-N=[0]*10000 # Array with 1000 elements 
-N[0]=1
-for i in range(1,10000):
-    t[i-1]=decayFunc(i,lambd)
+t=[0]*1000 
+N=[0]*1000 
+N[0]=2
+for i in range(1,1000):
+    [t[i-1],b[i-1],c[i-1]]=decayFunc(i,lambd)
     N[i]=N[i-1]+1
     N[i-1]=math.log(N[i-1],math.e)
-N[9999]=math.log(N[9999],math.e)
+N[999]=math.log(N[999],math.e)
 plt.plot(N,t,"bo")
 plt.ylabel("time")
 plt.xlabel("ln(N)")
+##problem 3
+plt.plot(b[50])
+plt.plot(b[997])
+##problem 4 
+plt.plot(c[221])
+plt.plot(c[997])
+##problem 5
+plt.plot(c[997],b[997])
+
 
